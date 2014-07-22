eb34ea70e2b741efcc925da0ef0afe35b13675ff
ivan-basekit <beekay@Gordons-MacBook-Pro-6.local>
2014-07-22 14:52:03 +0100

    test: removing document ready

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,3 @@
-$(document).ready(function() {
-	
-
-	$(document).on("click", '#navigationheader a.pull', function(e) {
-		$('body').toggleClass('navOpen');
-    });
+$(document).on("click", '#navigationheader a.pull', function(e) {
+	$('body').toggleClass('navOpen');
 });
